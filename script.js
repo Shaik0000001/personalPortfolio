f736296a5cc8403d80ea2b69fb650f895037368a
SHAIK MAHABOOBBASHA <mahaboobbashashaik671@gmail.com>
2026-03-04 22:50:03 +0530

    Add functions for resume download and project scrolling

---
+++ script.js
@@ -0,0 +1,19 @@
+function downloadResume(){
+
+alert("Resume download coming soon")
+
+}
+
+function scrollToProjects(){
+
+document.getElementById("projects").scrollIntoView({
+behavior:"smooth"
+})
+
+}
+
+function contactMe(){
+
+window.location.href="mailto:mahaboobbashashaik671@gmail.com"
+
+}
